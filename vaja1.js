19c9b0967910f413510e69062c7d29ce4f5a3c15
nikstambolija <121933862+nikstambolija@users.noreply.github.com>
2023-01-05 09:49:05 +0100

    Add files via upload

---
+++ vaja1.js
@@ -0,0 +1,20 @@
+function max_of_three(x, y, z) 
+ {
+  max_val = 0;
+  if (x > y)
+  {
+    max_val = x;
+  } else
+  {
+    max_val = y;
+  }
+  if (z > max_val) 
+  {
+    max_val = z;
+  }
+  return max_val;
+}
+
+console.log(max_of_three(x));
+console.log(max_of_three(y));
+console.log(max_of_three(z));
